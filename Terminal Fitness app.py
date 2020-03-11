41ead318fb88b5cf65e0fdf32949f3f262087cb8
Neil Sachdeva <44329583+Wiserlightning@users.noreply.github.com>
2020-03-10 23:29:20 -0400

    fixed errors

--- Terminal Fitness app.py
+++ Terminal Fitness app.py
@@ -1,2 +1,14 @@
-file_name = input("What do you want to call your fitness tracker? ")
-new_file = open(file_name+".txt", 'w') #creates a new file
+def new_file_and_exercise()
+    a = input("What do you want to call your fitness tracker? ")
+    new_file = open(a+".txt", 'w') #creates a new file
+    print("Created new file. ")
+    store = input("Input your exercise in the format: Muscle Group | Exercise Name | Sets| Weight | Reps | ")
+    print("Input stored.")
+    again = input("Do you want to enter another exercise. Yes or No.")
+    if again == 'No':
+        print("Thanks for using the exercise input app.")
+    else:
+        print("We will reset the app now so you can input another one.")
+        new_file_and_exercise()
+        
+new_file_and_exercise()
